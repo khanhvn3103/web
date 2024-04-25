b95b6ec0380d0b7bbde66a5f2bdc06f3f616045e
Khanh <khanhvn3103@gmail.com>
2024-04-25 12:33:01 +0700

    update login register

--- web-main/js/header.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  var dropdownLink = document.getElementById("navbarDarkDropdownMenuLink");
-  dropdownLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log("asdhkgfas");
-    window.location.href = "listProduct.html?pd=balo";
-  });
-}, 500);
